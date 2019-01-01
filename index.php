21300839b82aeb741bc48343dcd8401d89dae598
Rimantas55 <43905835+Rimantas55@users.noreply.github.com>
2019-01-01 19:36:51 +0200

    Some practice from tutorials and js file

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Some Practices</title>
+</head>
+<body>
+	<p>HTML input list even there you can search needed value</p>
+
+	<input list="colors" />
+	<datalist id="colors">
+		<option value="Blue">
+		<option value="Red">
+		<option value="Yellow">
+		<option value="Orange">
+		<option value="White">
+	</datalist>
+
+	<p>Date type</p>
+	<input type="datetime-local" /> <br>
+
+	<p>Numbers</p>
+	<input type="number" name="">
+
+	<p>Range ir naudoja JS</p>
+	<div>
+	<input type="range" 
+	name=""
+	id="move-range" 
+	min="0" max="120"
+	step="5" value="30"> 
+	
+	<span id="move-value">30</span>
+	</div>
+	<script>
+		var moveValue = document.getElementById('move-value');
+
+		document.getElementById('move-range').addEventListener('change', function(){
+			moveValue.innerText = this.value;
+		}, false);
+
+	</script>
+
+	<br>
+
+	<p>Kai naudojam inpute required + yra funkcija autofocus</p>
+	<input type="text" name="" required="" value="" autofocus="">
+
+	 <br />
+	 Drag and dromp js scripts
+	 <br />
+
+
+	 <script src="app.js"></script>
+
+
+</body>
+</html>
